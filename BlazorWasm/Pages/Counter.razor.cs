31efcdaaf5f1da6345d5427f2fe76da76118ebd5
kangdiancp <kangdiancp@gmail.com>
2023-03-07 12:32:34 +0700

    init project

---
+++ BlazorWasm/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorWasm.Pages
+{
+    public partial class Counter
+    {
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        private void DecrementCount()
+        {
+            if (currentCount > 0)
+                currentCount--;
+        }
+
+    }
+}
